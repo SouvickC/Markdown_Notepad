104adc1920d6b2eec0d3ed49edfc06d68679003b
SouvickC <souvickchakraborty100@gmail.com>
2025-04-26 14:52:50 +0200

    v3 commits

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,4 +1,4 @@
 chrome.action.onClicked.addListener((tab) => {
-    chrome.tabs.create({ url: chrome.runtime.getURL('index.html') });
+    chrome.tabs.create({ url: chrome.runtime.getURL('notepad_v3.html') });
   });
   
